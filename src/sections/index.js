7bea8977032e1c8c825b2e8792a3b992afb563ff
The White Hat KE <austinkings30@gmail.com>
2024-05-02 03:49:59 +0300

    first commit of nike react app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import Services from './Services'
+import Footer from './Footer'
+import SpecialOffer from './SpecialOffer'
+import PopularProducts from './PopularProducts'
+
+export {
+    Hero,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Services,
+    Footer,
+    SpecialOffer,
+    PopularProducts
+}
